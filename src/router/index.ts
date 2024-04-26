e8b9bf5bab68173debc1467695b344eb286d5268
yoguoer <1269148583@qq.com>
2024-04-26 20:39:02 +0800

    fix 🐛: 解决合并冲突。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
 import type { AppRouteModule } from "@/types/router";
 import type { setupRouterOptions } from "@/types/store";
+import type { App } from "vue";
 
 // 创建一个可以被 Vue 应用程序使用的路由实例
 export function toCreateRouter(
@@ -18,18 +19,22 @@ export function toCreateRouter(
 
 // 定义一个接口来描述函数需要的参数对象  
 
+function hasRouteraBeenSetup(app: App<Element>) {
+  return app.config.globalProperties.$router !== undefined;
+}
 
 // config router
 // 配置路由器
 export function setupRouter(rOptions: setupRouterOptions) {
   const { app, publicPath, asyncRoutes, basicRoutes } = rOptions;
-  let router = app.$router; // 尝试从应用实例上获取路由器
-  if (!router) {
+  let router; // 尝试从应用实例上获取路由器
+  if (!hasRouteraBeenSetup(app)) {
     // 如果没有设置router，则创建并使用它  
     router = toCreateRouter(publicPath, asyncRoutes, basicRoutes);  
     app.use(router);  
   } else {
     // 已经设置路由  
+    router = app.$router
     console.log('router has already been set up.');  
   }
   return router; // 无论app.$router是否存在，都会返回路由实例
