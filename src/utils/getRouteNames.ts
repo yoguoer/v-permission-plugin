2549488847dac2cae9e0fae78e59a64e0ed8c661
yoguoer <1269148583@qq.com>
2024-04-28 18:48:13 +0800

    fix 🐛: 重新打包发布。

--- src/utils/getRouteNames.ts
+++ src/utils/getRouteNames.ts
@@ -1,6 +1,6 @@
 import { getChildValue } from "@/utils/index"
 
-export default function gerRouteNames(data: any){
+export default function getRouteNames(data){
     let menuNames: Array<T> = []
      // 递归获取后端路由 name 的数组存入 leftMenuNames
      getChildValue(data?.menu || [], menuNames, 'name', 'children')
